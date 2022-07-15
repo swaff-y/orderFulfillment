c768b7093fb9e156022c4025e2a121326267f795
swaff-y <kyle@swaff.id.au>
2022-07-15 23:06:04 +1000

    doco on how the methods operate

--- processOrders.rb
+++ processOrders.rb
@@ -9,7 +9,13 @@ else
     return
 end
 
-
+# Purchase Order class
+# Accepts one parameter on initialization
+# Product (hash) The product that the purchase order must be created for
+# 3 methods:
+#   data => returns data hash
+#   create => returns purchase_order hash
+#   send => sends the purchase order returns the result
 class Purchase_order
     def initialize(product)
         @data = product
@@ -35,6 +41,27 @@ class Purchase_order
     end
 end
 
+
+# Fulfilment class
+# Accepts one parameter on initialization
+# hash (hash) data from the json file
+# 5 methods:
+#   get_orders => returns orders from a list it recieves (orders). If empty array, returns all orders
+#   get_products => returns the products from the data file
+#   get_product => returns a product from an item in an order (item)
+#   get_items => returns the items from an order (order)
+#   process_orders => returns order numbers that cannot be fulfilled
+#       1. iterates through orders
+#           a. iterate through each item on the order
+#           b. compare item with quantity on hand
+#           c. if it can be fulfilled add to fulfilled array and update stock
+#               b. check if reorder point is reached, create po if so and add it to the created PO array
+#           d. if it cant be fulfilled add to unfulfilled array
+#       2. for each order if the amount of items equals the items that can be fulfilled, chnage status to fulfilled
+#       3. for each order if the amount of items does not equal the items that can be fulfilled, chnage status to unfulfilled
+#       4. iterate through the purchase orders and send the POs that are unique
+#       5. create a new data file that is time stamped so it cannot mutate the orignal file
+#       6. return the array of orders that cannot be filfilled
 class Fulfilment
     def initialize(hash)
         @data = hash
@@ -93,39 +120,37 @@ class Fulfilment
                         if (product['quantityOnHand'] - item['quantity']) < product['reorderThreshold']
                             purchase_order = Purchase_order.new({ **product })
                             purchase_order.create
-                            po_data = purchase_order::data
                             
-                            # puts purchase_order::data['productId']
                             created_po = purchase_orders_created.find do |order|
                                 order::data['productId'] == purchase_order::data['productId']
-                            end
+                            end #purchase_orders_created.find
 
                             if !created_po
                                 purchase_orders_created.push(purchase_order)
                                 purchase_order.send
-                            end
-                        end
+                            end #if !created_po
+                        end #if (product['quantityOnHand'] - item['quantity']) < product['reorderThreshold']
                     else
                         items_that_cant_be_fulfilled.push(item)
-                    end
+                    end #if item['quantity'] <= product['quantityOnHand']
                 end #items.each
 
                 ret_order = @data['orders'].find do |ord|
                     ord['orderId'] == order['orderId']
-                end
+                end #@data['orders'].find
 
                 if items.length == items_that_can_be_fulfilled.length
                     items_that_can_be_fulfilled.each do |item|
                         product = get_product(item)
                         product['quantityOnHand'] = (product['quantityOnHand'] - item['quantity'])
-                    end
+                    end #items_that_can_be_fulfilled.each
 
                     ret_order['status'] = "Fulfilled";
                 else
                     #update status
                     orders_that_cannot_be_fulfiled.push(order['orderId'])
                     ret_order['status'] = "Unfulfillable"
-                end
+                end #if items.length == items_that_can_be_fulfilled.length
             end #if items
         end #orders.each
 
